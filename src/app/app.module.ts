f801556235504dc534dee8ea360f6726f8fae881
Taylor Page <jonathantaylorpage@gmail.com>
2018-05-03 22:04:08 -0700

    (chore) Install materialize
    - Install materialize dependency
    
    Signed-off-by: Taylor Page <jonathantaylorpage@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
