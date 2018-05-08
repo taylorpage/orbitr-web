4944a04c7f6f16d49abc1221d4856bbe1aeff698
Taylor Page <jonathantaylorpage@gmail.com>
2018-05-08 16:05:49 -0700

    (feat) Format slider labels
    - Add format to creation of noUi Sliders
    
    Signed-off-by: Taylor Page <jonathantaylorpage@gmail.com>

--- src/app/features/filters/filters.component.ts
+++ src/app/features/filters/filters.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 declare const noUiSlider;
+declare const wNumb;
 
 @Component({
   selector: 'app-filters',
@@ -10,22 +11,35 @@ declare const noUiSlider;
 export class FiltersComponent implements OnInit {
 
   @ViewChild('slider') slider: ElementRef;
+  @ViewChild('slider2') slider2: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
-    this.createNoUiSlider();
+    this.createNoUiSliders();
   }
 
-  createNoUiSlider() {
+  createNoUiSliders() {
     noUiSlider.create(this.slider.nativeElement, {
-      start: [0, 100],
+      start: [23, 45],
       connect: true,
       step: 1,
-      range: {
-        'min': 0,
-        'max': 100
+      range: { 'min': 18, 'max': 100 },
+      format: {
+        to: value => Math.ceil(value),
+        from: value => Math.ceil(value),
       }
      });
+
+    noUiSlider.create(this.slider2.nativeElement, {
+      start: [100],
+      step: 1,
+      connect: true,
+      range: { 'min': 0, 'max': 500 },
+      format: {
+        to: value => Math.ceil(value),
+        from: value => Math.ceil(value),
+      }
+    });
   }
 }
