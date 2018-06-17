5abc04ecea58940f6746808f7e4bde8a925fd79b
Taylor Page <jonathantaylorpage@gmail.com>
2018-06-17 12:21:54 -0700

    (chore) Update app to utilize heroku mongodb uri
    - Utilize the config variable supplid by heroku to access db
    
    Signed-off-by: Taylor Page <jonathantaylorpage@gmail.com>

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://127.0.0.1/orbitr_db';
+var mongoDB = process.env.MONGODB_URI || 'mongodb://127.0.0.1/orbitr_db';
 
 var connection = mongoose.createConnection(mongoDB, function(err, res) {
   var message = err ? 'MongoDB connection error' : 'MongoDB Connected';
